a71960b3d0dcb8e54245d06674e1b16ce8715c3b
marianaviro <m.villamizar564@uniandes.edu.co>
2018-06-01 01:12:11 -0500

    Minor bug

--- collective_challenges.js
+++ collective_challenges.js
@@ -16,38 +16,5 @@ var colective_challenges = [
     },
     {
       "name": "¡COMPARTE ESTA PÁGINA!"
-    },
-    {
-      "name": "Colectiva Libertaria Severas Flores"
-    },
-    {
-      "name": "Sisma Mujer"
-    },
-    {
-      "name": "La Mesa por la Vida y la Salud de las mujeres"
-    },
-    {
-      "name": "Las Parceras – Línea y Red Feminista de Acompañamiento en Aborto"
-    },
-    {
-      "name": "La Tremenda Revoltosa – Batucada Feminista"
-    },
-    {
-      "name": "La Morada"
-    },
-    {
-      "name": "Atrévete Bogotá Hollback!"
-    },
-    {
-      "name": "Observatorio Contra el Acoso Callejero"
-    },
-    {
-      "name": "Yerbateras"
-    },
-    {
-      "name": "Yela Quim"
-    },
-    {
-      "name": "MyGRLStory"
     }
   ];
